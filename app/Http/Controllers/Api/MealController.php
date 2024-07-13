adac2931ad7e2381b5405ec388f75e6e0ca6a6f8
SondosAbdelhameed <sondos.abdelhameed@yahoo.com>
2024-07-14 01:05:28 +0300

    try handel menu list

--- app/Http/Controllers/Api/MealController.php
+++ app/Http/Controllers/Api/MealController.php
@@ -7,16 +7,20 @@ use Illuminate\Http\Request;
 use App\Http\Resources\MealResource;
 use App\Models\Reservation;
 use App\Models\Meal;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
 
 class MealController extends Controller
 {
     public function listMenuItems() {
-        //$meal = Meal::withSum('todayOrderMeals','quantity')->get();
+        /*$meals = DB::table('meals as m')->select('m.id', 'm.available_quantity', DB::raw('SUM(ord.quantity) as day_count'))
+        ->leftjoin('order_details as ord','m.id' , '=' , 'ord.meal_id')
+        ->whereDate('ord.created_at',Carbon::today())
+        ->groupBy('m.id')->having('day_count' , '<' , 'm.available_quantity')->orHavingNull('day_count')->get();
+        return response()->json($meals);*/
+
         $meals = Meal::all();
-        //return $meal;
-        //return Meal::with('todayOrderMeals')->get();
-        //$meals = Meal::available()->get();
-        //$meals = new Meal();
         return MealResource::collection($meals);
     }
 }
